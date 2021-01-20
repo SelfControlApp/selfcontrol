8ddcfdbcfe320c1a2011c3fd6478bc549a07b3a2
Charlie Stigler <charlie@charliestigler.com>
2021-01-19 21:23:08 -0800

    Move utility methods from SCSettings to SCMiscUtilities

--- Common/Utility/SCMiscUtilities.h
+++ Common/Utility/SCMiscUtilities.h
@@ -15,6 +15,9 @@
 
 + (dispatch_source_t)createDebounceDispatchTimer:(double) debounceTime queue:(dispatch_queue_t)queue block:(dispatch_block_t)block;
 
++ (NSString *)getSerialNumber;
++ (NSString *)sha1:(NSString*)stringToHash;
+
 + (NSArray<NSString*>*) cleanBlocklistEntry:(NSString*)rawEntry;
 
 + (NSDictionary*) defaultsDictForUser:(uid_t)controllingUID;
