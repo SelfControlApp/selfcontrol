f9058593115b994098b7a8414ee6023e05144d98
Charlie Stigler <charlie@charliestigler.com>
2021-04-26 17:03:25 -0700

    Remove duration slider tick marks + other general UI cleanup

--- SCDurationSlider.m
+++ SCDurationSlider.m
@@ -42,45 +42,8 @@
 
 - (void)setMaxDuration:(NSInteger)maxDuration {
     _maxDuration = maxDuration;
-    [self recalculateSliderIntervals];
-}
-
-- (void)recalculateSliderIntervals {
     [self setMinValue: 1]; // never start a block shorter than 1 minute
     [self setMaxValue: self.maxDuration];
-    
-    // how many tick marks should we have? it's based on the max block duration
-    // TODO: can we make this work better with the start at 1-minute (vs 0)? if not, should we just eliminate ticks?
-    long tickInterval = 1;
-    if (self.maxDuration >= 5256000) {
-        // if max block duration is at least 10 years, tick marks are per-year
-        tickInterval = 525600;
-    } else if (self.maxDuration >= 525600) {
-        // if max block duration is at least 1 year, tick marks are per-month
-        tickInterval = 43800;
-    } else if (self.maxDuration >= 131400) {
-        // if max block duration is at least 3 months, tick marks are per-week
-        tickInterval = 10080;
-    } else if (self.maxDuration >= 10080) {
-        // if max block duration is at least 1 week, tick marks are per-day
-        tickInterval = 1440;
-    } else if (self.maxDuration >= 5760) {
-        // if max block duration is at least 4 days, tick marks are per 6 hours
-        tickInterval = 360;
-    } else if (self.maxDuration >= 1440) {
-        // if max block duration is at least 1 day, tick marks are per hour
-        tickInterval = 60;
-    } else if (self.maxDuration >= 720) {
-        // if max block duration is at least 12 hours, tick marks are per 30 minutes
-        tickInterval = 30;
-    } else if (self.maxDuration >= 60) {
-        // if max block duration is at least 1 hour, tick marks are per minute
-        tickInterval = 1;
-    }
-    
-    // no more than 72 ticks max
-    long numTicks = MIN(self.maxDuration / tickInterval, 72) + 1;
-    [self setNumberOfTickMarks: numTicks];
 }
 
 - (void)registerMinutesValueTransformer {
