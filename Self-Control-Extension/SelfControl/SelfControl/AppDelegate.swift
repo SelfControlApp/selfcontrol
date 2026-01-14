4b19eb3973064f31a31fc60815f3c80b9247fba6
Satendra Singh <satendra.techmac@gmail.com>
2026-01-14 21:04:32 +0530

    added app delegate to handle different app states

---
+++ Self-Control-Extension/SelfControl/SelfControl/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SelfControl
+//
+//  Created by Satendra Singh on 11/01/26.
+//
+
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var onAppClose: (() -> Void)?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        print("App did finish launching")
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        print("App will terminate")
+        onAppClose?()
+        // Save state, cleanup resources, stop services, etc.
+    }
+
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
+        print("Should terminate")
+        return .terminateNow
+    }
+}
